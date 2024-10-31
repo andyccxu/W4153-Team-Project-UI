02c76999b275a3c3144f90df2f597c386f2ecab8
Andy Xu <andyccxu@outlook.com>
2024-10-30 20:29:45 -0400

    Revert changes in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -10,10 +10,7 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-        process: 'readonly',
-      },
+      globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
